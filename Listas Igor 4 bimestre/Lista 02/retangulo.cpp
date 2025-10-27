55f2b89464010424aed0ca455e47dd92ae3e3fbd
pedro2630 <pedro.00136596@aluno.fiema.org.br>
2025-10-27 15:27:15 -0300

    Create retangulo.cpp

---
+++ Listas Igor 4 bimestre/Lista 02/retangulo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double base, altura, area, perimetro, diagonal;
+
+    cout << "Base do retangulo: ";
+    cin >> base;
+    cout << "Altura do retangulo: ";
+    cin >> altura;
+
+    area = base * altura;
+    perimetro = 2 * (base + altura);
+    diagonal = sqrt(base * base + altura * altura);
+
+    cout << fixed << setprecision(4);
+    cout << "Area = " << area << endl;
+    cout << "Perimetro = " << perimetro << endl;
+    cout << "Diagonal = " << diagonal << endl;
+
+    return 0;
+}
