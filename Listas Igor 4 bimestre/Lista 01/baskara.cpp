dd0aef7aa94ceb6ede8cd25fd5b604f802a3978c
pedro2630 <pedro.00136596@aluno.fiema.org.br>
2025-10-27 15:16:38 -0300

    Create baskara.cpp

---
+++ Listas Igor 4 bimestre/Lista 01/baskara.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath> // para sqrt()
+using namespace std;
+
+int main() {
+    double a, b, c;
+
+    cout << "Digite os valores de a, b e c: ";
+    cin >> a >> b >> c;
+
+    double delta = b*b - 4*a*c;
+
+    if (delta < 0) {
+        cout << "Esta equacao nao possui raizes reais." << endl;
+    } 
+    else {
+        double x1 = (-b + sqrt(delta)) / (2*a);
+        double x2 = (-b - sqrt(delta)) / (2*a);
+
+        cout << fixed << setprecision(4);
+        cout << "X1 = " << x1 << endl;
+        cout << "X2 = " << x2 << endl;
+    }
+
+    return 0;
+}
